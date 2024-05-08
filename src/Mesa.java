44f52e12c78322046743adf58936d51be77802b1
argenis <ikacsalcedo23@lhusurbil.eus>
2024-05-08 13:03:38 +0200

    commit argenis cambios mesa

--- src/Mesa.java
+++ src/Mesa.java
@@ -7,4 +7,11 @@ public class Mesa {
     private boolean ocupada;
     private boolean reservada;
     
+    public Mesa(int numero_mesa, Sala sala, boolean disponible, boolean se_puede_juntar, int capacidad){
+        this.numero_mesa = numero_mesa;
+        this.sala = sala;
+        this.disponible = disponible;
+        this.se_puede_juntar = se_puede_juntar;
+        this.capacidad = capacidad;
+    }
 }
