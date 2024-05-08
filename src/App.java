8b9e53b5f0e2b580eefd4b36478ce02f9adb1a53
argenis <ikacsalcedo23@lhusurbil.eus>
2024-05-08 12:55:39 +0200

    commit argenis prueba conflicto

--- src/App.java
+++ src/App.java
@@ -5,9 +5,9 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu(scanner);
-
+        // usuario administrador
         Usuario admin = new Usuario(true);
-
+        // usuario cliente
         Usuario cliente = new Usuario(false);
 
         menu.ejecutarMenu(admin);
